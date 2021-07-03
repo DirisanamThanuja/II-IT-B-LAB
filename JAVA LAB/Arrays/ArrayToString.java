e00189dc48155e86305af9bf6c73dc3fa8c37307
DirisanamThanuja <85677117+DirisanamThanuja@users.noreply.github.com>
2021-07-03 10:57:07 +0530

    Add files via upload

---
+++ JAVA LAB/Arrays/ArrayToString.java
@@ -0,0 +1,12 @@
+public class ArrayToString
+{
+   public static void main(String args[])
+   {
+      // Method 1: Using String object
+      char[] ch = {'T','h','a','n','u','j','a'};
+      String str = new String(ch);
+      System.out.println(str);
+ 
+      
+   }
+}
